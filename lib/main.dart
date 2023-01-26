a3a2b2c29a1942b7061c4a7738d4fbe634920870
halahakim119 <97341494+halahakim119@users.noreply.github.com>
2023-01-26 17:51:05 +0300

    connectivity plus for multi routes added

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,19 @@
 import 'package:flutter/material.dart';
-import '/my_home_page.dart';
 
-void main() {
-  runApp(MyApp());
+import 'injection_container.dart' as di;
+import 'my_home_page.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  // await di.init();
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'My App',
-      home: MyHomePage(),
-    );
+    return  MaterialApp(home: MyHomePage());
   }
 }
--- lib/my_home_page.dart
+++ lib/my_home_page.dart
@@ -1,9 +1,16 @@
 import 'dart:async';
-
-import 'package:flutter/material.dart';
-import 'package:connectivity_plus/connectivity_plus.dart';
-import 'package:flutter/services.dart';
 import 'dart:developer' as developer;
+
+import 'package:connectivity_plus/connectivity_plus.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+import 'core/routes/app_router.dart';
+import 'core/splash/no_internet_splash.dart';
+import 'home.dart';
+import 'injection_container.dart' as di;
+import 'injection_container.dart';
+import 'login_screen.dart';
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({
@@ -23,7 +30,7 @@ class _MyHomePageState extends State<MyHomePage> {
   void initState() {
     super.initState();
     initConnectivity();
-
+    // init();
     _connectivitySubscription =
         _connectivity.onConnectivityChanged.listen(_updateConnectionStatus);
   }
@@ -63,10 +70,31 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        body: _connectionStatus == ConnectivityResult.wifi
-            ? Center(child: Text('Connect: ${_connectionStatus.toString()}'))
-            : Center(
-                child: Text('not Connect: ${_connectionStatus.toString()}')));
+    // final appRouter = di.sl<AppRouter>();
+    return _connectionStatus == ConnectivityResult.wifi
+        ?   Navigator(onGenerateRoute: (settings) {
+            switch (settings.name) {
+              case '/':
+                return MaterialPageRoute(builder: (_) => Home());
+              case '/login':
+                return MaterialPageRoute(builder: (_) => LoginScreen());
+
+              default:
+                return null;
+              // MaterialPageRoute(
+              //     builder: (_) => Scaffold( 
+              //           body: Center(
+              //             child:
+              //                 Text('No route defined for ${settings.name}'),
+              //           ),
+              //         ));
+            }
+          })
+        // MaterialApp(
+        //     debugShowCheckedModeBanner: false,
+        //     title: 'iq project',
+        //     onGenerateRoute: appRouter.onGenerateRoute,
+        //   )
+        : const MaterialApp(home: NoInternetSplash());
   }
 }
