5c6c951b1f3d5ec1dbc3789c1ff160bf08e6161c
halahakim119 <97341494+halahakim119@users.noreply.github.com>
2023-02-12 08:46:09 +0300

    update

--- lib/features/schedule/presentation/view/daily/page/daily_schedule_screen.dart
+++ lib/features/schedule/presentation/view/daily/page/daily_schedule_screen.dart
@@ -1,3 +1,4 @@
+import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -6,24 +7,29 @@ import '../../../../../splash/splash_404.dart';
 import '../../../logic/cubit/schedule_cubit.dart';
 import '../widgets/daily_schedule_widget.dart';
 import '../widgets/food_state.dart';
+import '../widgets/weekend_widget.dart';
 
 class DailyScheduleScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    int day = DateTime.now().weekday;
     int hour = DateTime.now().hour;
 
-    return (hour >= 9 && hour < 10)
-        ? SingleChildScrollView(
-            child: BlocBuilder<ScheduleCubit, ScheduleState>(
-              builder: (context, state) {
-                return state.when(
-                  loading: () => const CustomLoading(),
-                  loaded: (schedule) => DailyScheduleWidget(schedule: schedule),
-                  error: (error) => Splash404(error: error),
-                );
-              },
-            ),
-          )
-        : const FoodState();
+    return (day == 5 || day == 6)
+        ? WeekendWidget()
+        : (hour >= 9 && hour < 10)
+            ? SingleChildScrollView(
+                child: BlocBuilder<ScheduleCubit, ScheduleState>(
+                  builder: (context, state) {
+                    return state.when(
+                      loading: () => const CustomLoading(),
+                      loaded: (schedule) =>
+                          DailyScheduleWidget(schedule: schedule),
+                      error: (error) => Splash404(error: error),
+                    );
+                  },
+                ),
+              )
+            : const FoodState();
   }
 }
